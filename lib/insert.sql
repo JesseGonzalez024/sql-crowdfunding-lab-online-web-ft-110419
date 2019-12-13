c99ea2ee61029537117c9be524f9b182c385455f
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-12-13 18:51:17 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+
+
+INSERT INTO users (name, age) 
+VALUES ('Jesse', 23), ('Gabriel', 24), ('Kevin', 23), ('Ariel', 30),
+('Sam', 16), ('Alison', 26), ('Melissa', 23), ('Adrian', 26), ('Isaac', 26), 
+('Andres', 55), ('Marina', 26), ('Mia', 24), ('Yohonson', 61), ('Eric', 54), 
+('Elain', 47), ('Jarred', 31), ('Victoria', 21), ('Alexis', 20), ('Chris' 26),
+('Jesse Dillon', 24);
